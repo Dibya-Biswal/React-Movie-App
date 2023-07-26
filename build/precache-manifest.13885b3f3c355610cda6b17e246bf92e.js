d8991ed25b7daafba629f825058ac4d67bf7846c
Dibya Biswal <121709187+Dibya-Biswal@users.noreply.github.com>
2023-07-26 15:11:30 +0530

    Add files via upload

---
+++ build/precache-manifest.13885b3f3c355610cda6b17e246bf92e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c28da63ec8b12e87f42",
+    "url": "/static/css/main.e96d5c76.chunk.css"
+  },
+  {
+    "revision": "1c28da63ec8b12e87f42",
+    "url": "/static/js/main.1c28da63.chunk.js"
+  },
+  {
+    "revision": "af7ce44480721190ab16",
+    "url": "/static/css/1.39377229.chunk.css"
+  },
+  {
+    "revision": "af7ce44480721190ab16",
+    "url": "/static/js/1.af7ce444.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f2c184b1c597d4f540d9c4d99cf5714b",
+    "url": "/index.html"
+  }
+];
